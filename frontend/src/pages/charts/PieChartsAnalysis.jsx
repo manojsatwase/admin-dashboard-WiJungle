c791decaf9546d4c839feced371090bc7d6e8c61
manoj_s <apple@manojsatwase.local>
2024-06-03 10:09:23 +0530

    pie charts analysis

---
+++ frontend/src/pages/charts/PieChartsAnalysis.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MyTabs from '../../components/MyTabs';
+
+const PieChartsAnalysis = () => {
+  return (
+      <>
+      <MyTabs /> 
+      </>
+  );
+};
+
+export default PieChartsAnalysis;
